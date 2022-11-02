f1b34fb5eac4810fc625bbce5c52fce7a7fa3068
gmngmstr <shaunmartin1998@yahoo.com>
2022-11-02 16:17:00 -0700

    Initial Upload of classes and test file

---
+++ Iridium/__init__.py
@@ -0,0 +1,57 @@
+import serial
+from Logger import Logger
+from adafruit_rockblock import RockBlock
+
+
+class Iridium(RockBlock):
+    def __init__(self, port="/dev/serial0", baud=19200, debug=False):
+        self.logger = Logger("RockBlock", debug)
+        self.log("Initiating: ", "Rock Block with " + str(port) + ", " + str(baud) + ", and debugging " + str(debug))
+        self.uart = serial.Serial(port, baud)
+        super().__init__(self.uart)
+        self.log("Model: ", str(self.model))
+        self.log("System Time: ", str(self.system_time))
+        self.retry = 0
+        self.data = ""
+
+    def log(self, tag, message):
+        log = self.logger
+        log.log(tag, message)
+
+    def sq(self):
+        sq = self.signal_quality
+        self.log("Signal Quality: ", sq)
+        return sq
+
+    def available(self):
+        return self.uart.in_waiting
+
+    def read(self):
+        self.data = self.uart.readline()
+        return self.data
+
+    def receive(self):
+        self.data = self.data_in
+        # self.retry = 0
+        return self.data
+
+    def check_signal_status(self):
+        return self.satellite_transfer()
+
+    def check_messages(self):
+        if self.available() > 0:
+            temp_message = []
+            while self.available() > 0:
+                temp = self.receive()
+                # temp = self.read()
+                temp_message.append(temp.decode())
+            return temp_message
+
+    def send(self, msg):
+        self.log("Message: ", msg)
+        self.data_out(msg)
+
+    def write(self, msg):
+        self.log("Message: ", msg)
+        self.uart.write(msg)
+
